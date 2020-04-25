a024ec8fe3e26fe38c033217921cbd2c44a4eff2
ouardirhi <lamyaa.ouardirhi@gmail.com>
2020-04-25 14:05:38 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webvoyage::Application.config.secret_token = '0d3d0aa3f67f671b7721cabec4a06c3d824f70684479386ca61dbf1e0781c74b5c3e7a033642d1ae1ee9e150e9d724dce8fe977f014f8d517f8a96624a879db6'
